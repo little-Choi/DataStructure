64958c0a2680afdbd8e208a4784df478e3724e79
little-Choi <dkssudsdkssu@gmail.com>
2023-06-26 21:53:38 +0900

    vector 구현 1. 생성자, 소멸자 2. 인덱스 연산자 오버로딩 3. push_back 4. resize 5. data 6. size 7. capacity 8. clear

--- DataStructure/CArray.h
+++ DataStructure/CArray.h
@@ -4,9 +4,78 @@ template<typename T>
 class CArr
 {
 private:
-	T* data;
+	T* pData;
 	int data_count;
 	int max_count;
 
 public:
+	CArr()
+		: pData(nullptr)
+		, data_count(0)
+		, max_count(0)
+	{
+		pData = new T[2];
+		max_count = 2;
+	}
+
+	~CArr()
+	{
+		if (pData)
+		{
+			delete[] pData;
+			pData = nullptr;
+		}
+	}
+
+public:
+	T& operator [] (int idx)
+	{
+		return pData[idx];
+	}
+
+public:
+	void push_back(const T& new_data);
+	void resize(int resize_count);
+	T* data() { return pData; }
+	int size() { return data_count; }
+	int capacity() { return max_count; }
+	void clear();
+
 };
+
+template<typename T>
+inline void CArr<T>::push_back(const T& new_data)
+{
+	if (data_count >= max_count)
+	{
+		resize(max_count * 2);
+	}
+
+	pData[data_count++] = new_data;
+}
+
+template<typename T>
+inline void CArr<T>::resize(int resize_count)
+{
+	T* new_data = new T[resize_count];
+
+	for (int i = 0; i < data_count; ++i)
+	{
+		new_data[i] = pData[i];
+	}
+	delete[] pData;
+	max_count = resize_count;
+	pData = new_data;
+}
+
+template<typename T>
+inline void CArr<T>::clear()
+{
+	if (pData)
+	{
+		delete[] pData;
+		pData = nullptr;
+	}
+	data_count = 0;
+	max_count = 0;
+}
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
+#include "CArray.h"
+
+using std::vector;
 
 int main()
 {
+	// vector ±¸Çö
+	{
+		vector<int> vecInt;
+		vecInt.push_back(10);
+		vecInt.push_back(20);
+		vecInt.push_back(30);
+		vecInt.resize(10);
 
+		int* a1 = vecInt.data();
+		int a2 = vecInt[1];
+		int a3 = vecInt.size();
 
+		CArr<int> arrInt;
+		arrInt.push_back(10);
+		arrInt.push_back(20);
+		arrInt.push_back(30);
+		arrInt.resize(10);
+		int* b1 = arrInt.data();
+		int b2 = arrInt[1];
+		int b3 = arrInt.size();
+		arrInt.clear();
+
+		int k = 0;
+	}
 
 
 

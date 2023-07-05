0775bc341ca8cca8716c022ea38efdd0f8eb8e34
little-Choi <dkssudsdkssu@gmail.com>
2023-07-05 22:11:06 +0900

    CBST 1. enum class 활용하여 3노드 한번에 구현

--- DataStructure/CBST.h
+++ DataStructure/CBST.h
@@ -2,12 +2,19 @@
 
 #include <cassert>
 
+enum class NODE_TYPE
+{
+	PARENT, // 0
+	LCHILD, // 1
+	RCHILD, // 2
+	END,	// 3
+};
+
 template<typename T1, typename T2>
 struct tPair
 {
 	T1 first;
 	T2 second;
-
 };
 
 template<typename T1, typename T2>
@@ -21,22 +28,16 @@ template<typename T1, typename T2>
 struct tBSTNode
 {
 	tPair<T1, T2> pair;
-	tBSTNode* parent_node;
-	tBSTNode* lchild_node;
-	tBSTNode* rchild_node;
+	tBSTNode* arr_node[(int)NODE_TYPE::END];
 
 	tBSTNode()
 		: pair()
-		, parent_node(nullptr)
-		, lchild_node(nullptr)
-		, rchild_node(nullptr)
+		, arr_node{ nullptr, nullptr, nullptr}
 	{}
 
 	tBSTNode(const tPair<T1, T2> _pair, tBSTNode* _parent_node, tBSTNode* _lchild_node, tBSTNode* _rchild_node)
 		: pair(_pair)
-		, parent_node(_parent_node)
-		, lchild_node(_lchild_node)
-		, rchild_node(_rchild_node)
+		, arr_node{ _parent_node, _lchild_node, _rchild_node }
 	{}
 };
 
@@ -76,28 +77,28 @@ inline bool CBST<T1, T2>::insert(const tPair<T1, T2>& _pair)
 		{
 			if (_pair.first > node->pair.first)
 			{
-				if (nullptr == node->rchild_node)
+				if (nullptr == node->arr_node[(int)NODE_TYPE::RCHILD])
 				{
-					node->rchild_node = new_node;
-					new_node->parent_node = node;
+					node->arr_node[(int)NODE_TYPE::RCHILD] = new_node;
+					new_node->arr_node[(int)NODE_TYPE::PARENT] = node;
 					break;
 				}
 				else
 				{
-					node = node->rchild_node;
+					node = node->arr_node[(int)NODE_TYPE::RCHILD];
 				}
 			}
 			else if (_pair.first < node->pair.first)
 			{
-				if (nullptr == node->lchild_node)
+				if (nullptr == node->arr_node[(int)NODE_TYPE::LCHILD])
 				{
-					node->lchild_node = new_node;
-					new_node->parent_node = node;
+					node->arr_node[(int)NODE_TYPE::LCHILD] = new_node;
+					new_node->arr_node[(int)NODE_TYPE::PARENT] = node;
 					break;
 				}
 				else
 				{
-					node = node->lchild_node;
+					node = node->arr_node[(int)NODE_TYPE::LCHILD];
 				}
 			}
 			else
